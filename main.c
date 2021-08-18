0a20f6903a58a5f1c3c0f7223eafd2573eb84c83
John Lonergan <john.lonergan@gmail.com>
2021-08-18 13:23:41 +0100

    separated I2C from NS stuff

--- main.c
+++ main.c
@@ -5,9 +5,11 @@
  */
 
 #include "mcc_generated_files/mcc.h"
-typedef unsigned char result_t;
+#include "i2c.h"
+#include "controller.h"
 
-void output(result_t out) {
+
+void output(uint8_t out) {
     RAND_0_LAT = (out & 1);
     RAND_1_LAT = (out & 2) >> 1;
     RAND_2_LAT = (out & 4) >> 2;
@@ -19,102 +21,27 @@ void output(result_t out) {
 
 }
 
-void pause() {
-    //__delay_us(1);
-}
-//
-//void nesInit() {
-//    // set clock to inactive low state before starting transfer
-//    NES_CLK_SetLow();
-//    NES_LATCH_SetLow();
-//}
-//
-//void nesLatch() {
-//    // load controller's 4021 shift register on +ve edge of latch
-//    NES_LATCH_SetHigh();
-//    NES_LATCH_SetLow();
-//}
-//
-//void nesClock() {
-//    NES_CLK_SetHigh();
-//    NES_CLK_SetLow();
-//}
-//
-//void rcvInit() {
-//    // set clock to inactive low state before starting transfer
-//    OUT_CLK_SetLow();
-//    OUT_LATCH_SetLow();    
-//}
-//
-//void rcvLatch() {
-//    // 74595 latches data into output reg on +ve edge
-//    // https://assets.nexperia.com/documents/data-sheet/74HC_HCT595.pdf
-//    OUT_LATCH_SetHigh();
-//    OUT_LATCH_SetLow();    
-//}
-//
-//void rcvClock() {
-//    OUT_CLK_SetHigh();
-//    OUT_CLK_SetLow();
-//}
-//
-//// see https://tresi.github.io/nes/ for signalling logic
-//void nesTransfer() {
-//   
-//    nesInit();
-//    rcvInit();
-//    
-//    nesLatch();
-//    rcvClock();
-//    
-//    nesClock();
-//    rcvClock();
-//    
-//    nesClock();
-//    rcvClock();
-//    
-//    nesClock();
-//    rcvClock();
-//    
-//    nesClock();
-//    rcvClock();
-//    
-//    nesClock();
-//    rcvClock();
-//    
-//    nesClock();
-//    rcvClock();
-//    
-//    nesClock();
-//    rcvClock();
-//
-//    rcvLatch();
-//}
-
 void randUpdate() {
-    result_t r = (result_t)(rand() % 256);
+    uint8_t r = (uint8_t)(rand() % 256);
     output(r);
 }
 
-
-
-void i2cDemo(); 
-void i2cInit(); 
-uint16_t nes_get_state();
-
 void main(void) {
     SYSTEM_Initialize();
-
-    i2cInit();
+    i2cSetup();
+    controller_disable_encryption();
+    controller_init();
     
+    uint8_t id[6];
+    controller_id(id);
+
     while (1) {
 
-        //nesTransfer();
-        //i2cDemo(); 
-//        randUpdate();
-        uint16_t state = nes_get_state();
-        uint16_t x = state;
-    }
+        //randUpdate();
+        
+        uint8_t state = controller_state(); 
+        output(state ^ 0xff); 
+   }
 }
 
 
